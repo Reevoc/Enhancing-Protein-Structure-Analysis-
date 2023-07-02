8de90b8b9773fec9e39c2ba892297c182fa46a25
Piermarco Giustini <61749102+Reevoc@users.noreply.github.com>
2023-07-02 12:47:51 +0200

    starting file

--- confix.py
+++ confix.py
@@ -1,2 +1,3 @@
 # Path to data Files, path to external tools and hyperparameters
 PATH_DSSP = "hssp-3.1.5" # Path to DSSP executable
+PATH_FEATURES_RING = 'data/features_ring/'
--- hssp-3.1.5/src/hssp-convert-3to1.h
+++
@@ -1,8 +0,0 @@
-#ifndef HSSP_CONVERT_3TO1_H
-#define HSSP_CONVERT_3TO1_H
-
-#include <iostream>
-
-void ConvertHsspFile(std::istream& in, std::ostream& out);
-
-#endif // HSSP_CONVERT_3TO1_H
